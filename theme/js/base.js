4a21b2bd4908fb28a8343048a50bf146a85382f2
M1sery <m0x0is3ry@foxmail.com>
2023-10-28 18:18:33 +0800

    Update base.js

--- theme/js/base.js
+++ theme/js/base.js
@@ -1,12 +1,13 @@
-fetch("http://124.70.33.170:3000/vip", {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Cookie': document.cookie,
-  },
-})
-  .then(response => response.json())
-  .then(data => {
-  })
-  .catch(error => {
-  });
+var xhr = new XMLHttpRequest();
+var url = 'http://localhost:3000/vip';
+var data = 'param1=value1&param2=value2'; // 请求参数
+
+xhr.open('POST', url, true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    console.log(response);
+  }
+};
+xhr.send(data);
