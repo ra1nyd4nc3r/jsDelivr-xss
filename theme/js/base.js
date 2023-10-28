506227c67a55c33180d1d5df7a7942ab7b65f6ac
M1sery <m0x0is3ry@foxmail.com>
2023-10-28 18:22:11 +0800

    Update base.js

--- theme/js/base.js
+++ theme/js/base.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
-var url = 'http://localhost:3000/vip';
-var data = 'param1=value1&param2=value2'; // 请求参数
+var url = 'http://127.0.0.1:3000/vip';
+var data = 'param1=value1&param2=value2';
 
 xhr.open('POST', url, true);
 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
